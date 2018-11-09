ccf91f44d771f83c931258303d3ec6e58ab631ce
coderparadise <2077551879@qq.com>
2018-11-09 15:08:00 +0800

    test

---
+++ provider-user/src/main/java/cn/tedu/ProviderRunApp1.java
@@ -0,0 +1,14 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProviderRunApp1 {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderRunApp1.class, args);
+	}
+}
